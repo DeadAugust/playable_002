609f46c2a4df80d89e74571f50ac4059cd602e7f
DeadAugust <augustluhrs@gmail.com>
2018-12-02 16:40:23 -0500

    changed path

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile('public/index.html')
 });
 
 http.listen(port, function(){
