484cd5c3bd51f8a1b011456f6cf32e6e7c0e31e4
DeadAugust <augustluhrs@gmail.com>
2018-12-02 16:45:15 -0500

    switch to static

--- server.js
+++ server.js
@@ -62,9 +62,11 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html')
-});
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/public/index.html')
+// });
+app.use(express.static('public'));
+
 
 http.listen(port, function(){
   console.log('listening on ' + port);
